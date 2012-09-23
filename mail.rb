7c7786a999099ff99535ffa823ea7046cc6c4fa5
Eric Farkas <eric@prudentiadigital.com>
2012-09-23 18:13:29 -0300

    Do not save message_id

--- mail.rb
+++ mail.rb
@@ -4,7 +4,7 @@ require 'gmail'
 gmail = Gmail.new(EMAIL_CONFIG['email'], EMAIL_CONFIG['password'])
 
 gmail.inbox.emails.each do |email|
-  IncomingMessage.create(:message_id => email.message_id, :message_body => email.parts[0].body.decoded)
+  IncomingMessage.create(:message_body => email.parts[0].body.decoded)
   email.delete!
 end
 
