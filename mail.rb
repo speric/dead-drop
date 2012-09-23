2053631000343302b4ae9aebc854e575644cbd82
Eric Farkas <eric@prudentiadigital.com>
2012-09-23 18:15:38 -0300

    Don't store message_id

--- mail.rb
+++ mail.rb
@@ -1,10 +1,12 @@
+require 'rubygems'
 require './config/environment.rb'
 require 'gmail'
 
 gmail = Gmail.new(EMAIL_CONFIG['email'], EMAIL_CONFIG['password'])
 
 gmail.inbox.emails.each do |email|
-  IncomingMessage.create(:message_body => email.parts[0].body.decoded)
+  message_body = email.parts[0].nil? ? email.body : email.parts[0].body.decoded
+  IncomingMessage.create(:message_body => message_body)
   email.delete!
 end
 
